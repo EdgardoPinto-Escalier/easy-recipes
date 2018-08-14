06753c9284ef384e4e768a4107d5fabe2682e0bd
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-08-14 16:43:54 +0200

    Updates on Index component

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="hello">
-
+  <div class="index container">
+    <div class="card" v-for="recipe in recipes" :key="recipe.id">
+      <div class="card-content">
+        <h2 class="purple-text">{{ recipe.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in recipe.ingredients" :key="index">
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,13 +18,27 @@ export default {
   name: 'Index',
   data () {
     return {
-
+      recipes: [
+        { title: 'Pasta Recipe', slug: 'pasta-recipe', ingredients: ['Pasta', 'Tomatoes', 'Salt'],id: '1' },
+        { title: 'Valencian Paella', slug: 'valencian-paella', ingredients: ['rice', 'fish', 'Vegetables'], id: '2' }
+      ]
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
 
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0px;
+}
 </style>
