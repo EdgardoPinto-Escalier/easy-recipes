1e80db85de9b17dd32a7b2eff2565136aa65b300
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-08-14 22:05:02 +0200

    updated Index.vue

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,6 +2,7 @@
   <div class="index container">
     <div class="card" v-for="recipe in recipes" :key="recipe.id">
       <div class="card-content">
+        <i class="material-icons delete" @click="deleteRecipe(recipe.id)">delete</i>
         <h2 class="purple-text">{{ recipe.title }}</h2>
         <ul class="ingredients">
           <!-- Here we loop through the ingredients using v-for -->
@@ -15,15 +16,36 @@
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
   name: 'Index',
   data () {
     return {
-      recipes: [
-        { title: 'Pasta Recipe', slug: 'pasta-recipe', ingredients: ['Pasta', 'Tomatoes', 'Salt'],id: '1' },
-        { title: 'Valencian Paella', slug: 'valencian-paella', ingredients: ['rice', 'fish', 'Vegetables'], id: '2' }
-      ]
+      recipes: []
     }
+  },
+  methods: {
+    deleteRecipe(id){
+      // Delete doc from firestore
+      db.collection('recipes').doc(id).delete()
+      .then(() => {
+        this.recipes = this.recipes.filter(recipe => {
+          return recipe.id != id
+        })
+      }) 
+    }
+  },
+  created(){
+    // Here we get the data from firestore.
+    db.collection('recipes').get() // We reference and get all the info for the collection.
+    .then(snapshot => { // We return a snapshot (a view of the collection at this moment)
+      snapshot.forEach(doc => { // We cicle through the snapshot and grab each document.
+        let recipe = doc.data() // We grab the data from the object and assign it to a variable.
+        recipe.id = doc.id // We add the id.
+        this.recipes.push(recipe) // We push it to our array recipes []
+      })
+    })
   }
 }
 </script>
@@ -51,5 +73,14 @@ export default {
   display: inline-block;
   text-transform: uppercase;
   font-weight: bold;
+}
+
+.index .delete {
+  position: absolute;
+  top: 6px;
+  right: 6px;
+  cursor: pointer;
+  color: #aaaaaa;
+  font-size: 1.8em;
 }
 </style>
